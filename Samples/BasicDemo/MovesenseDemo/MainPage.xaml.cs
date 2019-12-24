37e86d3c57ce20402defff6c3298102ce384cfe5
moljac <mcvjetko@holisticware.net>
2019-12-24 11:55:46 +0100

    BasicDemo

--- Samples/BasicDemo/MovesenseDemo/MainPage.xaml.cs
+++ Samples/BasicDemo/MovesenseDemo/MainPage.xaml.cs
@@ -39,6 +39,7 @@ namespace MovesenseDemo
 
         private void DoScan()
         {
+            bluetooth_devices = new List<IDevice>();
             StatusLabel.Text = "Scanning for devices...";
             scan = this.BleAdapter.Scan()
             .Subscribe(this.OnScanResult);
@@ -49,14 +50,22 @@ namespace MovesenseDemo
             this.scan?.Dispose();
         }
 
+        List<IDevice> bluetooth_devices = null;
         async void OnScanResult(IScanResult result)
         {
             // Only interested in Movesense devices
             if (result.Device.Name != null)
             {
+                if (result.Device.Name.StartsWith("Polar H"))
+                {
+                    StopScanning();
+                    bluetooth_devices.Add(result.Device);
+                    StatusLabel.Text = $"Scan found device {result.Device.Name}" + Environment.NewLine + StatusLabel.Text;
+                }
                 if (result.Device.Name.StartsWith("Movesense"))
                 {
                     StopScanning();
+                    bluetooth_devices.Add(result.Device);
 
                     var movesense = Plugin.Movesense.CrossMovesense.Current;
 
@@ -67,19 +76,26 @@ namespace MovesenseDemo
 
                     // Now do the Mds connection
                     var sensor = result.Device;
-                    StatusLabel.Text = $"Connecting to device {sensor.Name}";
+                    StatusLabel.Text = $"Connecting to device {sensor.Name}" + Environment.NewLine + StatusLabel.Text;
                     var movesenseDevice = await movesense.ConnectMdsAsync(sensor.Uuid);
 
                     // Talk to the device
-                    StatusLabel.Text = "Getting device info";
+                    StatusLabel.Text = "Getting device info" + Environment.NewLine + StatusLabel.Text;
                     var info = await movesenseDevice.GetDeviceInfoAsync();
 
-                    StatusLabel.Text = "Getting battery level";
+                    StatusLabel.Text = "Getting battery level" + Environment.NewLine + StatusLabel.Text;
                     var batt = await movesenseDevice.GetBatteryLevelAsync();
 
                     // Turn on the LED
-                    StatusLabel.Text = "Turning on LED";
-                    await movesenseDevice.SetLedStateAsync(0, true);
+                    StatusLabel.Text = "Turning on LED" + Environment.NewLine + StatusLabel.Text;
+                    try
+                    {
+                        await movesenseDevice.SetLedStateAsync(0, true);
+                    }
+                    catch(System.Exception exc)
+                    {
+                        StatusLabel.Text = $"Turning ON LED error {exc.Message}" + Environment.NewLine + StatusLabel.Text;
+                    }
 
                     await DisplayAlert(
                         "Success", 
@@ -87,13 +103,20 @@ namespace MovesenseDemo
                         "OK");
 
                     // Turn the LED off again
-                    StatusLabel.Text = "Turning off LED";
-                    await movesenseDevice.SetLedStateAsync(0, false);
+                    StatusLabel.Text = "Turning off LED" + Environment.NewLine + StatusLabel.Text;
+                    try
+                    {
+                        await movesenseDevice.SetLedStateAsync(0, false);
+                    }
+                    catch (System.Exception exc)
+                    {
+                        StatusLabel.Text = $"Turning OFF LED error {exc.Message}"+ Environment.NewLine + StatusLabel.Text;
+                    }
 
                     // Disconnect Mds
-                    StatusLabel.Text = "Disconnecting";
+                    StatusLabel.Text = "Disconnecting" + StatusLabel.Text;
                     await movesenseDevice.DisconnectMdsAsync();
-                    StatusLabel.Text = "Disconnected";
+                    StatusLabel.Text = "Disconnected  + StatusLabel.Text";
 
                 }
             }
